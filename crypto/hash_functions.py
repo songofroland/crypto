d29045b1c1dc8333b89f24470560567a247667e0
songofroland <rolandj@protonmail.com>
2020-12-06 20:51:46 +0100

    Update hash functions

--- crypto/hash_functions.py
+++ crypto/hash_functions.py
@@ -1,24 +1,48 @@
 import hashlib
+import random
+import string
+import time
+from pprint import pprint
+
 
 hash_funcs = {
-    "1": hashlib.md5,
-    "2": hashlib.sha1,
-    "3": hashlib.sha256,
-    "4": hashlib.sha384,
-    "5": hashlib.sha512,
-    "6": hashlib.sha3_224,
-    "7": hashlib.sha3_256,
-    "8": hashlib.sha3_384,
-    "9": hashlib.sha3_512,
+    "MD5": hashlib.md5,
+    "SHA-1": hashlib.sha1,
+    "SHA2(256)": hashlib.sha256,
+    "SHA2(384)": hashlib.sha384,
+    "SHA2(512)": hashlib.sha512,
+    "SHA-3(224)": hashlib.sha3_224,
+    "SHA-3(256)": hashlib.sha3_256,
+    "SHA-3(384)": hashlib.sha3_384,
+    "SHA-3(512)": hashlib.sha3_512,
 }
 
-while True:
-    method = 0
-    while method not in hash_funcs:
-        method = input(
-            "Please select hash method:"
-            "\n1 - MD5\n2 - SHA-1\n3 - SHA2(256)\n4 - SHA2(384)\n5 - SHA2(512)\n"
-            "6 - SHA-3(256)\n7 - SHA-3(384)\n8 - SHA-3(512)\n"
-        )
-    data = input("Plese enter word to hash: ")
-    print(f"\nCalculated hash: {hash_funcs[method](data.encode()).hexdigest()}\n")
+
+def get_hash_and_time(method: str, data: bytes) -> tuple:
+    start = time.time()
+    result = hash_funcs[method](data).hexdigest()
+    return result, time.time() - start
+
+
+FREE_INPUT = 1
+MODE = FREE_INPUT
+
+run = True
+while run:
+    results = {}
+    if MODE == FREE_INPUT:
+        data = input("Enter the text to hash: ")
+        for name, func in hash_funcs.items():
+            results[name] = {}
+            result, seconds = get_hash_and_time(name, data.encode())
+            results[name]["hash"] = result
+            results[name]["time"] = f"{seconds} seconds."
+    else:
+        for name, func in hash_funcs.items():
+            results[name] = {}
+            for size in ("small", "medium", "large"):
+                with open(f"{size}.txt", "rb") as f:
+                    results[name][size] = get_hash_and_time(name, f.read())[1]
+        run = False
+
+    pprint(results)
